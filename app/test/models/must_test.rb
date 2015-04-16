11550ecb4804deab9f01173e259fd7966d6eabb3
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-16 11:41:42 +0200

    Added test for calculating the correct rating

--- app/test/models/must_test.rb
+++ app/test/models/must_test.rb
@@ -8,6 +8,7 @@ describe User do
     @user = User.create(username: 'Mira', password: '123')
     @must = Must.create(name: 'Extra', brand: 'Failsafe', manufacturer: 'Okända Industries', package: 'Bottle', year: 1990, location: 'Stockholm, Finland')
     @review = Review.create(title: 'Welcoming', rating: 5, user_id: 1, must_id: 1, content: 'pretium a lacus. In malesuada ultricies tellus nec elementum. Morbi quis volutpat mi. Vestibulum sit amet purus leo. Etiam bibendum, leo non vehicula mollis, nibh massa eleifend ante, blandit aliquet justo metus et leo.')
+    @review = Review.create(title: 'Welcoming', rating: 2, user_id: 1, must_id: 1, content: 'pretium a lacus. In malesuada ultricies tellus nec elementum. Morbi quis volutpat mi. Vestibulum sit amet purus leo. Etiam bibendum, leo non vehicula mollis, nibh massa eleifend ante, blandit aliquet justo metus et leo.')
   end
 
   describe 'associations' do
@@ -16,6 +17,12 @@ describe User do
       expect( @must.reviews.first.title ).to match 'Welcoming'
     end
 
+  end
+
+  describe 'rating' do 
+    it 'should calculate the correct rating' do 
+      expect( @must.calculate_rating ).eq 3.5
+    end
   end
 
 end
