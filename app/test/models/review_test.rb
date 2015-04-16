eb9cec243c5ffb7cd7a03ef0d858545717a704be
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-16 10:42:03 +0200

    Association tests for models

---
+++ app/test/models/review_test.rb
@@ -0,0 +1,25 @@
+require_relative 'models_helper'
+
+describe User do
+
+  before do
+    DataMapper.auto_migrate!
+
+    @user = User.create(username: 'Mira', password: '123')
+    @must = Must.create(name: 'Extra', brand: 'Failsafe', manufacturer: 'Okända Industries', package: 'Bottle', year: 1990, location: 'Stockholm, Finland')
+    @review = Review.create(title: 'Welcoming', rating: 5, user_id: 1, must_id: 1, content: 'pretium a lacus. In malesuada ultricies tellus nec elementum. Morbi quis volutpat mi. Vestibulum sit amet purus leo. Etiam bibendum, leo non vehicula mollis, nibh massa eleifend ante, blandit aliquet justo metus et leo.')
+  end
+
+  describe 'associations' do
+
+    it 'should return the correct user' do
+      expect( @review.user.username ).to match 'Mira'
+    end
+
+    it 'should return the correct must' do 
+      expect( @review.must.name).to match 'Extra'
+    end
+
+  end
+
+end
---
+++ app/test/models/user_test.rb
@@ -0,0 +1,21 @@
+require_relative 'models_helper'
+
+describe User do
+
+  before do
+    DataMapper.auto_migrate!
+
+    @user = User.create(username: 'Mira', password: '123')
+    @must = Must.create(name: 'Extra', brand: 'Failsafe', manufacturer: 'Okända Industries', package: 'Bottle', year: 1990, location: 'Stockholm, Finland')
+    @review = Review.create(title: 'Welcoming', rating: 5, user_id: 1, must_id: 1, content: 'pretium a lacus. In malesuada ultricies tellus nec elementum. Morbi quis volutpat mi. Vestibulum sit amet purus leo. Etiam bibendum, leo non vehicula mollis, nibh massa eleifend ante, blandit aliquet justo metus et leo.')
+  end
+
+  describe 'associations' do
+
+    it 'should return the correct review' do
+      expect( @user.reviews.first.title ).to match 'Welcoming'
+    end
+
+  end
+
+end
