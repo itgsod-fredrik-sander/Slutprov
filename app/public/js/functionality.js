e6c3a15d556f371ca6b47da440350a487c4cd0f8
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-17 10:56:30 +0200

    Added commenting for javascript functions

--- app/public/js/functionality.js
+++ app/public/js/functionality.js
@@ -7,12 +7,34 @@ $('#review-toggler').click(function() {
   toggleText($(this), 'Hide', 'Write a review!');
 });
 
+// Public: toggleVisibility of element
+//
+// element - the element to be toggled
+//
+// Examples
+//
+//   toggleVisibility($('#map'));
+//
+// Toggles the visibility of the element
+
 function toggleVisibility(element) {
   var display = element.css('display');
   var new_state = display == 'block' ? 'none' : 'block';
 
   element.css('display', new_state);
 }
+
+// Public: toggleText from element
+//
+// element - the element that will have it's text toglged
+// original - the original text
+// toglged - the text to be shown when toggled
+//
+// Examples
+//
+//   toggleText($(#input), 'View reviews!', 'Hide reviews');
+//
+// Toggles the elements text
 
 function toggleText(element, original, toggled) {
   var currentText = element.text();
