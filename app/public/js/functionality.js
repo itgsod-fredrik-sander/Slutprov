d6b6940c367d292f024803041b59eec7c73fcf74
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-17 10:43:54 +0200

    Hiding and toggling the review input bar

--- app/public/js/functionality.js
+++ app/public/js/functionality.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
     $('#must-list').DataTable();
-} );
+});
+
+$('#review-toggler').click(function() {
+  toggleVisibility($('#new-review'));
+  toggleText($(this), 'Hide', 'Write a review!');
+});
+
+function toggleVisibility(element) {
+  var display = element.css('display');
+  var new_state = display == 'block' ? 'none' : 'block';
+
+  element.css('display', new_state);
+}
+
+function toggleText(element, original, toggled) {
+  var currentText = element.text();
+  var new_text = currentText == original ? toggled : original
+
+  element.text(new_text);
+}
