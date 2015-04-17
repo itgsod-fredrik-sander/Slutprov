320985d752444f1a29d9feb1bddbd11f4dddcb65
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-17 11:01:33 +0200

    Added commenting for models

--- app/models/Must.rb
+++ app/models/Must.rb
@@ -11,6 +11,16 @@ class Must
 
   has n, :reviews
 
+  # Public: Calculating rating of must instance
+  #
+  # must - the must's rating to be calculated
+  #
+  # Examples
+  #
+  # Must.calculate_rating(@must) => 3.5
+  #
+  # Calculates the rating of the element
+
   def self.calculate_rating(must)
     sum, divider = 0, must.reviews.size
     must.reviews.each {|review| sum += review.rating}
--- app/models/User.rb
+++ app/models/User.rb
@@ -7,6 +7,17 @@ class User
 
   has n, :reviews
 
+  # Public: Attempts to login the user
+  #
+  # params - The hash which contains login details
+  #
+  # Examples
+  #
+  # Requirements: User with username 'mira' and password 'mira123'
+  # User.login({username: 'mira', password: 'mira123'}) => true
+  #
+  # Attempts to login the user
+
   def self.login(params)
     @user = User.first(:username => params[:username])
     @user && @user.password == params[:password] ? true : false
